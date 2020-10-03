b48ee352799e1204669db762eca83b17e5157aa9
viveksinghscheduleonce <viveksingh101994@gmail.com>
2020-10-03 22:38:08 +0530

    added subscription

--- src/graphql/resolvers/Subscription.js
+++ src/graphql/resolvers/Subscription.js
@@ -1,3 +1,5 @@
+const { getPostById } = require('../../db/queries/post');
+
 const Subscription = {
   count: {
     subscribe(parent, args, { pubsub }) {
@@ -11,6 +13,20 @@ const Subscription = {
       }, 1000);
 
       return pubsub.asyncIterator('count');
+    },
+  },
+  comment: {
+    subscribe: async (parent, { postId }, { pubsub }) => {
+      const post = await getPostById(postId);
+      if (!post) {
+        return new Error('Post not found');
+      }
+      return pubsub.asyncIterator(`comment_${postId}`);
+    },
+  },
+  post: {
+    subscribe: async (parent, args, { pubsub }) => {
+      return pubsub.asyncIterator(`post`);
     },
   },
 };
